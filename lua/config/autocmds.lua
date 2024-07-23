98d74306592a51b8d86b05bc2c17e6b9efd43ac8
nvimcoder <nvimcoder@qq.com>
2024-07-24 07:42:35 +0800

    feat: restore cursor shape on exit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "ExitPre" }, {
+  callback = function()
+    -- vim.opt.guicursor = "a:ver30-blinkon1-blinkoff250-blinkwait10"
+    vim.cmd('set guicursor= | call chansend(v:stderr, "\x1b[ q")')
+  end,
+})
